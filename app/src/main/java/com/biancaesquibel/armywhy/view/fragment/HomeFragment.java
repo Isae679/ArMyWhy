df7b3060079ca06007786f99c837ca260a75ab06
Blanca Isabel Esquibel Loayza <blancaisabel4321@gmail.com>
2021-09-16 02:07:27 -0400

    ----RecyclerView----

--- app/src/main/java/com/biancaesquibel/armywhy/view/fragment/HomeFragment.java
+++ app/src/main/java/com/biancaesquibel/armywhy/view/fragment/HomeFragment.java
@@ -2,13 +2,22 @@ package com.biancaesquibel.armywhy.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.biancaesquibel.armywhy.R;
+import com.biancaesquibel.armywhy.adapter.CardViewAdapter;
+import com.biancaesquibel.armywhy.model.Image;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +70,51 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+       //toolbar
+        showToolBar(getString(R.string.title_home_fragment),false, view);
+
+        //recyclerview
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+
+        //layout manager
+         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //el adapter
+        CardViewAdapter cardViewAdapter = new CardViewAdapter(buidImages(), R.layout.cardview_image, getActivity());
+        recyclerView.setAdapter(cardViewAdapter);
+
+        return view;
+    }
+
+
+    public void showToolBar(String titulo, boolean botonSubir, View view){
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(titulo);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+
+    //creamos la lista de imagenes
+    public ArrayList<Image> buidImages(){
+        ArrayList<Image> images = new  ArrayList<>();
+
+        images.add(new Image("","kevin Salazar", "2 dias", "1 me gusta"));
+        images.add(new Image("","Luis Peres", "3 dias", "5 me gusta"));
+        images.add(new Image("","Fernando Torrico", "6 dias", "4 me gusta"));
+        images.add(new Image("","Nicol Mendoza", "5 dias", "7 me gusta"));
+        images.add(new Image("","Alexander Lara", "3 dias", "3 me gusta"));
+        images.add(new Image("","Maissel Valdivia", "4 dias", "9 me gusta"));
+        images.add(new Image("","William Jimenez", "2 dias", "6 me gusta"));
+        images.add(new Image("","Roberta Montaño", "2 dias", "2 me gusta"));
+        images.add(new Image("","Vivia Ledezma", "6 dias", "3 me gusta"));
+        images.add(new Image("","Rafael Rojas", "5 dias", "1 me gusta"));
+
+
+        return images;
     }
 }
